b131b58bbe1924e04c4b8a7f02ca5a1f54e9d3a4
userYashDOB <yashdob.2005@gmail.com>
2024-07-02 19:47:38 +0530

    Init

---
+++ src/app/page.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <body className="body">
+      <div className="scroll-watcher"></div>
+      <div className="headerimg">
+        <div className="head">
+          Hey Guys Welcome to Animation World 😊
+        </div>
+      </div>
+      <div className="view">
+        <div className="block block1"></div>
+        <div className="block block2"></div>
+        <div className="block block3"></div>
+        <div className="block block4"></div>
+        <div className="block block5"></div>
+        <div className="block block6"></div>
+      </div>
+
+    </body>
+  );
+}
